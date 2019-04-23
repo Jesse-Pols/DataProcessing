5fe06caf4c3d9967a170de4baac8251853cf2fc8
Jesse Pols <jesse@truepeople.nl>
2019-04-23 14:39:26 +0200

    ReizigersApp opdracht, op basis van arraylist

---
+++ src/ReizigerApp/ReizigerDao.java
@@ -0,0 +1,14 @@
+package ReizigerApp;
+
+import java.util.List;
+
+public interface ReizigerDao {
+	
+	public List<Reiziger> findAll();
+	public List<Reiziger> findByGBdatum(String gbdatum);
+	
+	public Reiziger save(Reiziger reiziger);
+	public Reiziger update(Reiziger reiziger);
+	public boolean delete(Reiziger reiziger);
+	
+}
